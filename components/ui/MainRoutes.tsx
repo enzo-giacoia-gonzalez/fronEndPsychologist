b13521e51b831e54e8a0dd11ea4abaec91e76e4b
EVZODA <enzogiacoia@hotmail.com>
2024-11-01 20:50:29 -0300

    Main routes modified

--- components/ui/MainRoutes.tsx
+++ components/ui/MainRoutes.tsx
@@ -56,25 +56,25 @@ const MainRoutes = () => {
         <Routes>
             <Route path='/' element={<Start/>}/>
             <Route path='*' element={<Error404/>}/>
-            <Route path='/CourseProgram' element={token && rol=="PATIENT_ROLE" || rol=="ADMIN_ROLE"?<CourseProgram/>:<Navigate to= "/"/>}/>
-            <Route path='/MyCourses' element={token && rol=="PATIENT_ROLE" || rol=="ADMIN_ROLE"?<MyCourses/>:<Navigate to= "/"/>}/> 
-            <Route path='/PsychologySessions' element={rol==="USER_ROLE" || rol==="PATIENT_ROLE" && token?<PsychologySessions/>:<Navigate to= "/"/>}/>
-            <Route path='/Login' element={!token?<Login/>:""}/>
-            <Route path='/Register' element={!token?<Register/>:""}/>
-            <Route path='/ForgetPassword' element={!token?<ForgetPassword/>:<Navigate to= "/"/>}/>
-            <Route path='/resetpassword/:token' element={!token?<ResetPasswordParams/>:""} />
-            <Route path='/UserList' element={rol==="ADMIN_ROLE" && token?<UserList/>:<Navigate to= "/"/>}/>
-            <Route path='/Classes/AddVideos' element={rol==="ADMIN_ROLE" && token?<AddClasses/>:<Navigate to= "/"/>}/>
-            <Route path='/Classes/ModifyClasses' element={rol==="ADMIN_ROLE" && token?<ModifyClasses/>:<Navigate to= "/"/>}/>
-            <Route path='/Pay' element={rol=="PATIENT_ROLE" || rol=="USER_ROLE" && token ?<Pay/>:<Navigate to= "/"/>}/>
-            <Route path='/PayItem' element={rol=="PATIENT_ROLE" || rol=="USER_ROLE" && token ?<PayItem/>:<Navigate to= "/"/>}/>
-            <Route path='/HistorialPaymentAdmin' element={rol==="ADMIN_ROLE" && token?<HistorialPaymentAdmin/>:<Navigate to= "/"/>}/>
-            <Route path='/HistorialPaymentUser' element={rol==="PATIENT_ROLE" || rol==="USER_ROLE" && token ?<HistorialPaymentUser/>:<Navigate to= "/"/>}/>
-            <Route path='/PayItemService' element={rol=="ADMIN_ROLE" && token?<PayItemService/>:<Navigate to= "/"/>}/>
-            <Route path='/PatientCalendar' element={rol=="ADMIN_ROLE" && token?<PatientCalendar/>:<Navigate to= "/"/>}/>
-            <Route path='/MoreNotifications' element={<MoreNotifications/>}/>
-            <Route path='/ReceiptPayment' element={rol=="ADMIN_ROLE" && token?<ReceiptPayment/>:<Navigate to= "/"/>}/>
-            <Route path='/PaySessionCv' element={rol=="PATIENT_ROLE" || rol=="USER_ROLE" && token ?<PaySessionCv/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/CourseProgram' element={token && rol=="PATIENT_ROLE" || rol=="ADMIN_ROLE"?<CourseProgram/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/MyCourses' element={token && rol=="PATIENT_ROLE" || rol=="ADMIN_ROLE"?<MyCourses/>:<Navigate to= "/"/>}/> 
+            <Route path='https://fron-end-psychologist.vercel.app/PsychologySessions' element={rol==="USER_ROLE" || rol==="PATIENT_ROLE" && token?<PsychologySessions/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/Login' element={!token?<Login/>:""}/>
+            <Route path='https://fron-end-psychologist.vercel.app/Register' element={!token?<Register/>:""}/>
+            <Route path='https://fron-end-psychologist.vercel.app/ForgetPassword' element={!token?<ForgetPassword/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/resetpassword/:token' element={!token?<ResetPasswordParams/>:""} />
+            <Route path='https://fron-end-psychologist.vercel.app/UserList' element={rol==="ADMIN_ROLE" && token?<UserList/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/Classes/AddVideos' element={rol==="ADMIN_ROLE" && token?<AddClasses/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/Classes/ModifyClasses' element={rol==="ADMIN_ROLE" && token?<ModifyClasses/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/Pay' element={rol=="PATIENT_ROLE" || rol=="USER_ROLE" && token ?<Pay/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/PayItem' element={rol=="PATIENT_ROLE" || rol=="USER_ROLE" && token ?<PayItem/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/HistorialPaymentAdmin' element={rol==="ADMIN_ROLE" && token?<HistorialPaymentAdmin/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/HistorialPaymentUser' element={rol==="PATIENT_ROLE" || rol==="USER_ROLE" && token ?<HistorialPaymentUser/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/PayItemService' element={rol=="ADMIN_ROLE" && token?<PayItemService/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/PatientCalendar' element={rol=="ADMIN_ROLE" && token?<PatientCalendar/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/MoreNotifications' element={<MoreNotifications/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/ReceiptPayment' element={rol=="ADMIN_ROLE" && token?<ReceiptPayment/>:<Navigate to= "/"/>}/>
+            <Route path='https://fron-end-psychologist.vercel.app/PaySessionCv' element={rol=="PATIENT_ROLE" || rol=="USER_ROLE" && token ?<PaySessionCv/>:<Navigate to= "/"/>}/>
         </Routes>
         <Footer></Footer>
     </div>
